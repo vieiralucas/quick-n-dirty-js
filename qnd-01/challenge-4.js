e440dabb2a5267afc9c1f56cc8f232524e337ff3
Lucas Vieira <vieiralucas4@gmail.com>
2014-11-05 19:33:37 -0200

    finish qnd-01/challenge-4

--- qnd-01/challenge-4.js
+++ qnd-01/challenge-4.js
@@ -8,9 +8,13 @@ var foo = function() {
             console.log("false");
         }
     }
-
+    console.dir(bar);
     return bar;
 };
 
 var fn = foo();
 fn(); // wtf?!
+
+// @vieiralucas
+// Declaração de funções ocorrem antes da execução do script
+// Logo a declaração de dentro do else sobrescreve a declaração de dentro do if
