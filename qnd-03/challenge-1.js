243f404bebf07a6a54b1dde2f4e90ef4927d9c7a
Lucas Vieira <vieiralucas4@gmail.com>
2014-11-05 23:44:14 -0200

    finish qnd-03/challenge-1

--- qnd-03/challenge-1.js
+++ qnd-03/challenge-1.js
@@ -1,1 +1,34 @@
 // implementar uma "classe" com atributos/métodos privados
+
+// @vieiralucas
+
+var Classe = function() {
+
+	this.bar = 'bar'; // atributo publico
+	var foo = 'foo'; // atributo privado
+
+	// metodos privados
+	var bar = function() {
+		foo = 'bar';
+	}
+
+	// métodos publicos
+	this.getFoo = function() {
+		return foo;
+	};
+	this.setFoo = function(newFoo) {
+		foo = newFoo;
+	};
+	this.toBar = function() {
+		return bar();
+	};
+}
+
+var obj = new Classe();
+
+console.log(obj.bar);
+console.log(obj.getFoo());
+obj.setFoo('bar');
+console.log(obj.getFoo());
+obj.toBar();
+console.log(obj.getFoo());
