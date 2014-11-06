5b8e187f8a6d0da8ce49a9338939c3688f57e5fe
Lucas Vieira <vieiralucas4@gmail.com>
2014-11-05 23:44:29 -0200

    finish qnd-03/challenge-2

--- qnd-03/challenge-2.js
+++ qnd-03/challenge-2.js
@@ -1,1 +1,23 @@
-// implementar duas classes, sendo que uma é herança de outra, e a classe "filha" deverá sobreescrever um método da classe "pai"
+// implementar duas classes, sendo que uma é herança de outra, e a classe 'filha" deverá sobreescrever um método da classe "pai"
+
+// @vieiralucas
+// classe pai
+var Animal = function(nome, sexo) {
+	this.nome = nome;
+	this.sexo = sexo;
+}
+
+Animal.prototype.ola = function() {
+	console.log('Goar!');
+};
+
+// classe filha
+var Humano = function(nome, sexo) {
+	Animal.call(this, nome, sexo);
+}
+
+Humano.prototype = Object.create(Animal.prototype);
+
+Humano.prototype.ola = function() {
+	console.log('Olá, meu nome é ' + this.nome + '!');
+};
