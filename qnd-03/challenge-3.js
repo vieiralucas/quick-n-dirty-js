216f85411a77f2b316fec2b954aba55a3608eadd
Lucas Vieira <vieiralucas4@gmail.com>
2014-11-05 23:53:20 -0200

    qnd-03/challenge-3

--- qnd-03/challenge-3.js
+++ qnd-03/challenge-3.js
@@ -1,1 +1,33 @@
 // implementar duas classes, sendo que uma é herança de outra, e a classe "filha" deverá chamar um método da classe "pai"
+
+// @vieiralucas
+// classe pai
+var Animal = function(nome, sexo, idade) {
+	this.nome = nome;
+	this.sexo = sexo;
+	this.idade = idade;
+}
+
+Animal.prototype.ola = function() {
+	console.log('Goar!');
+};
+
+Animal.prototype.aniversario = function() {
+	return ++this.idade;
+};
+
+// classe filha
+var Humano = function(nome, sexo, idade) {
+	Animal.call(this, nome, sexo, idade);
+}
+
+Humano.prototype = Object.create(Animal.prototype);
+
+Humano.prototype.ola = function() {
+	console.log('Olá, meu nome é ' + this.nome + '!');
+};
+
+Humano.prototype.aniversario = function() { // que criativo --'
+	console.log(this.nome + " acaba de fazer aniversário!! \\o/");
+	return Animal.prototype.aniversario.call(this);
+};
